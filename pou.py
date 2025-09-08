7c74f7eeca0b57897fa6b24435fff91f87458188
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-09-08 16:32:24 +0000

    rename and remove databases

--- pou.py
+++ pou.py
@@ -26,7 +26,7 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="firs tim meh"))
     await client.tree.sync()
 
-@client.hybrid_command()
+@client.hybrid_command(description="connect to a database")
 async def connect_database(ctx, database:str):
     global db_connection
     global db_cursor
@@ -34,7 +34,7 @@ async def connect_database(ctx, database:str):
     db_cursor = db_connection.cursor()
     await ctx.send(f"connecting to {database}")
 
-@client.hybrid_command()
+@client.hybrid_command(description="executes a SQL command")
 @app_commands.choices(commit=[
     app_commands.Choice(name="true", value="true"),
     app_commands.Choice(name="false", value="false")
@@ -50,12 +50,12 @@ async def exec_command(ctx, *, command:str, commit:str="false"):
     await ctx.send(f"executing: `{command}`")
     await ctx.send(f"```{msg}```")
 
-@client.hybrid_command()
+@client.hybrid_command(description="commit all changes in the current transactions")
 async def commit_changes(ctx):
     db_connection.commit()
     await ctx.send("changes committed")
 
-@client.hybrid_command()
+@client.hybrid_command(description="list all databases")
 async def list_databases(ctx):
     databases_list = os.listdir(db_folder)
     for database in databases_list:
@@ -64,6 +64,26 @@ async def list_databases(ctx):
 
     msg = "\n".join(databases_list)
     await ctx.send(msg)
+
+@client.hybrid_command(description="deletes a database")
+async def delete_database(ctx, database:str):
+    try:
+        os.remove(f"{db_folder}/{database}.db")
+        await ctx.send(f"ok removed {database}.db")
+    except Exception as e:
+        await ctx.send(e)
+
+@client.hybrid_command(description="renames a database")
+async def rename_database(ctx, original_name:str, new_name:str):
+    if original_name and new_name:
+        try:
+            os.rename(f"{db_folder}/{original_name}.db", f"{db_folder}/{new_name}.db")
+            await ctx.send(f"ok renamed {original_name}.db to {new_name}.db, pls use /connect_database again if connected to the old database")
+        except Exception as e:
+            await ctx.send(e)
+
+    else:
+        await ctx.send("um pls provide both names")
 
 
 bot_id_list = [1186326404267266059, 839794863591260182, 944245571714170930, 1396935480284680334, 1414634216292876308]
